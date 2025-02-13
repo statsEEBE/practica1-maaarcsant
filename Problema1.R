2b45afc4cc34794222bc7945e5aa60f0ff5e9b3d
marc.santos.uribe <marc.santos.uribe@auleseebe.upc.edu>
2025-02-13 13:03:08 +0100

    hola

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,41 @@
 #Codigo para problema 1
 
-plot(1:5)
+mtcars
+dim(mtcars)
+
+mis_dades <-mtcars
+
+mis_dades
+#propedio
+mean(mtcars$qsec)
+
+x<-mis_dades$qsec
+sum(x)/length(x)
+
+
+x<-mis_dades$drat
+
+sort(x)
+#mediana
+median(x)
+
+quantile(x, 0.25)
+quantile(x, 0.75)
+
+
+quantile(mis_dades$mpg, 0.18)
+
+quantile(mis_dades$mpg, 0.75)-quantile(mis_dades$mpg, 0.25)
+IQR(mis_dades$mpg)
+
+boxplot(mis_dades$mpg)
+
+mean(mis_dades$cyl)
+IQR(mis_dades$cyl)
+
+#desviacion tipica corregida
+sd(mis_dades$cyl)
+
+#la varianncia corregida
+var(mis_dades$qsec)
+aa
